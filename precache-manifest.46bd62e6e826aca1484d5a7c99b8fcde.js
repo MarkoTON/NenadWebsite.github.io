d6bf89f67bf841e2a5dc56c36d18f8cd6e516b70
MarkoTON <markoTONcanovic@gmail.com>
2020-02-05 17:43:45 +0100

    Deploy

---
+++ precache-manifest.46bd62e6e826aca1484d5a7c99b8fcde.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "90de685b16a5759a3913",
+    "url": "/css/app.2509a82a.css"
+  },
+  {
+    "revision": "90de685b16a5759a3913",
+    "url": "/js/app.85311532.js"
+  },
+  {
+    "revision": "b5ef877272a1f096fd5b",
+    "url": "/js/chunk-vendors.ff3dd05d.js"
+  },
+  {
+    "revision": "a4c33c0baa29303579e13d6655d546fd",
+    "url": "/img/link-symbol.a4c33c0b.svg"
+  },
+  {
+    "revision": "78588c89b8d62f49baa060834515a0db",
+    "url": "/img/artificial-intelligence.78588c89.svg"
+  },
+  {
+    "revision": "503ae2c2e98d11eeabafa4ae905a97c9",
+    "url": "/img/architectural-design-architecture-buildings-936722.503ae2c2.jpg"
+  },
+  {
+    "revision": "bbfddc869468b73f3b928c92a418940f",
+    "url": "/img/close-envelope.bbfddc86.svg"
+  },
+  {
+    "revision": "e7f358afba384257580a3083aba8b260",
+    "url": "/img/header-photo.e7f358af.jpeg"
+  },
+  {
+    "revision": "60e04946b99395ecdc91bab877959766",
+    "url": "/img/landscape-night-sing.60e04946.jpg"
+  },
+  {
+    "revision": "7611e71fcb68e173b69503e5c306a785",
+    "url": "/img/tick-inside-circle.7611e71f.svg"
+  },
+  {
+    "revision": "daf9323460a7af5ace7214ca8cf5589e",
+    "url": "/media/klipc-demo.daf93234.mp4"
+  },
+  {
+    "revision": "f465755fe6ff8977e9a8bbe8fd0bbed6",
+    "url": "/img/phone.f465755f.svg"
+  },
+  {
+    "revision": "1669bdec86d72bbf17776aa40a786f5f",
+    "url": "/img/handshake-small.1669bdec.jpg"
+  },
+  {
+    "revision": "a68f5cbcddb434ebda481d0f82dc73c3",
+    "url": "/img/logo-cropped.a68f5cbc.png"
+  },
+  {
+    "revision": "97691228fede8c974b28e9620e4f847a",
+    "url": "/img/question-mark-buble.97691228.svg"
+  },
+  {
+    "revision": "58a4a86701f6717cf8a60596b4ef16f8",
+    "url": "/index.html"
+  }
+];
